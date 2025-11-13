42da5e5e3545aafddf2b3c1391802b68019bbb39
Igubara <ramakusuma111111@gmail.com>
2025-11-13 10:33:18 +0800

    update tabel user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,19 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('level');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('level')->default('user'); 
             $table->rememberToken();
             $table->timestamps();
-        });
-    }
+    });
+}
 
     /**
      * Reverse the migrations.
